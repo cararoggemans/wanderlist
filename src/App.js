94bd3f2d3bd9c9397880ff7ebf8d0d0796c2b973
RoggemansC <Cara.Roggemans@acolad.com>
2022-12-10 13:05:09 +0100

    add logo & image startup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import WanderlistLogoV1 from "./assets/svg/wanderlist-logo-v1.svg"
+import ImgGlobe from "./assets/img/globe-start.jpg"
+
 function App() {
   return (
     <>
-      
+    <div className="section__top p-4">
+    <img className='rounded-2xl' src={ImgGlobe} alt='Globe start'/>
+    </div>
+    <div className='logo mx-auto flex flex-wrap items-center justify-center mt-8'>
+      <img className='h-12 w-auto' src={WanderlistLogoV1} alt='Wanderlist Logo' />
+      <h1 className='text-xl text-xl pl-2 font-semibold'>Wanderlist</h1>
+    </div>
     </>
-  );
+  )
 }
 
 export default App;
